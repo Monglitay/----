bde7c11dc37a8f1ad2b18fb0dbcff6b9107f499b
Monglitay <2811924088>
2025-04-27 18:57:07 +0800

    update

---
+++ project_1_TI_Learning/temple/delay/delay.h
@@ -0,0 +1,30 @@
+#ifndef _DELAY_H_
+#define _DELAY_H_
+
+
+#include "ti_msp_dl_config.h"
+
+
+void delay_ms(unsigned int sx);
+void delay_us(unsigned int sx);
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
